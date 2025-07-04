9a449570dfb8367d75a25ce32c436df19677152e
William Park <williamsleepark@gmail.com>
2025-07-04 16:59:24 -0400

    add jenkins to launch devbox

--- devbox.sh
+++ devbox.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=devbox
 #SBATCH --output="/vast/wlp9800/logs/%x-%j.out"
 #SBATCH --error="/vast/wlp9800/logs/%x-%j.err"
-#SBATCH --time=06:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=512M
+#SBATCH --mem=256M
 
 curl -sL https://raw.githubusercontent.com/thewillyP/jenkins/main/update_dns.sh | bash -s devbox
 
