be47119ee1701219c7e2688b22d42343495d6425
William Park <williamsleepark@gmail.com>
2025-07-04 15:52:50 -0400

    fix?

--- devbox.sh
+++ devbox.sh
@@ -7,24 +7,24 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=512M
 
-# Create hostkeys dir and generate RSA host key
-mkdir -p ~/hostkeys
-ssh-keygen -q -N "" -t rsa -b 4096 -f ~/hostkeys/ssh_host_rsa_key <<< y
 
-# Read DNS from your file
+# Get DNS IP from file
 DNS_IP=$(< ~/willyp_ip.txt)
 
-# Run the container using singularity
+# Get public key from current user
+PUBKEY=$(cat ~/.ssh/id_rsa.pub)
+
+# Username = whoever submitted this script
+USERNAME="$USER"
+
+# Run container via singularity with all proper envs
 singularity exec \
-  --bind ~/.ssh \
+  --env PUBLIC_KEY="$PUBKEY" \
+  --env USER_NAME="$USERNAME" \
+  --env TZ=UTC \
+  --env PUID=$(id -u) \
+  --env PGID=$(id -g) \
   --dns "$DNS_IP" \
-  docker://linuxserver/openssh-server \
-  /usr/sbin/sshd -D -p 2222 \
-    -o PermitUserEnvironment=yes \
-    -o PermitTTY=yes \
-    -o X11Forwarding=yes \
-    -o AllowTcpForwarding=yes \
-    -o GatewayPorts=yes \
-    -o ForceCommand=/bin/bash \
-    -o UsePAM=no \
-    -h ~/hostkeys/ssh_host_rsa_key
+  --bind ~/.ssh:/config/.ssh \
+  docker://lscr.io/linuxserver/openssh-server \
+  /init
