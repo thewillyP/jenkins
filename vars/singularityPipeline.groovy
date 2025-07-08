c04815d2575a6bf97fdb6d0010b8aa9d39fc8796
thewillyP <williamsleepark@gmail.com>
2025-07-08 14:41:11 -0400

    test

--- vars/singularityPipeline.groovy
+++ vars/singularityPipeline.groovy
@@ -23,16 +23,15 @@ def call(Map params) {
                     ssh -o StrictHostKeyChecking=no ${SSH_USER}@${EXEC_HOST} \
                         env AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID} \
                             AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY} \
-                            AWS_DEFAULT_REGION=us-east-1 '
+                            AWS_REGION=us-east-1 '
                         mkdir -p /tmp/scripts
                         curl -fsSL ${SCRIPT_BASE_URL}/cancel_jobs.sh -o /tmp/scripts/cancel_jobs.sh
                         curl -fsSL ${SCRIPT_BASE_URL}/cancel_jobs.sh.sig -o /tmp/scripts/cancel_jobs.sh.sig
                         singularity run \
-                            --env AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID},AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY},AWS_DEFAULT_REGION=\${AWS_DEFAULT_REGION} \
+                            --env AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID},AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY},AWS_REGION=\${AWS_REGION} \
                             docker://amazon/aws-cli ssm get-parameter \
                             --name "/gpg/public-key" \
                             --with-decryption \
-                            --region "us-east-1" \
                             --query Parameter.Value \
                             --output text > /tmp/scripts/public.key
                         gpg --import /tmp/scripts/public.key
