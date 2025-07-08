b3efd0eaf7aad232e462772655a57df58ad6ea80
thewillyP <williamsleepark@gmail.com>
2025-07-08 14:14:25 -0400

    try again

--- vars/singularityPipeline.groovy
+++ vars/singularityPipeline.groovy
@@ -25,7 +25,7 @@ def call(Map params) {
                         mkdir -p /tmp/scripts
                         curl -fsSL ${SCRIPT_BASE_URL}/cancel_jobs.sh -o /tmp/scripts/cancel_jobs.sh
                         curl -fsSL ${SCRIPT_BASE_URL}/cancel_jobs.sh.sig -o /tmp/scripts/cancel_jobs.sh.sig
-                        singularity exec --env AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID},AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY} docker://amazon/aws-cli aws ssm get-parameter \
+                        singularity run --env AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID},AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY} docker://amazon/aws-cli ssm get-parameter \
                         --name "/gpg/public-key" \
                         --with-decryption \
                         --region us-east-1 \
