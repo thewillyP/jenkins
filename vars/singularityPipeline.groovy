b05b392eca43066cb769bec7f9efe360401d0a3c
thewillyP <williamsleepark@gmail.com>
2025-07-08 14:53:45 -0400

    test

--- vars/singularityPipeline.groovy
+++ vars/singularityPipeline.groovy
@@ -20,33 +20,28 @@ def call(Map params) {
             stage('Cancel Existing Jobs') {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-credentials', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                     sh """
-                    ssh -o StrictHostKeyChecking=no ${SSH_USER}@${EXEC_HOST} \
-                        env AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID} \
-                            AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY} \
-                            AWS_DEFAULT_REGION=us-east-1 '
+                    ssh -o StrictHostKeyChecking=no ${SSH_USER}@${EXEC_HOST} \\
+                    env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \\
+                        AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \\
+                        AWS_DEFAULT_REGION=us-east-1 "
                         mkdir -p /tmp/scripts
                         curl -fsSL ${SCRIPT_BASE_URL}/cancel_jobs.sh -o /tmp/scripts/cancel_jobs.sh
                         curl -fsSL ${SCRIPT_BASE_URL}/cancel_jobs.sh.sig -o /tmp/scripts/cancel_jobs.sh.sig
-                        singularity exec \
-                            --cleanenv \
-                            --env AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID},AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY},AWS_DEFAULT_REGION=\${AWS_DEFAULT_REGION} \
-                            docker://amazon/aws-cli \
-                            aws ssm get-parameter \
-                            --name "/gpg/public-key" \
-                            --with-decryption \
-                            --region us-east-1 \
-                            --query Parameter.Value \
-                            --output text > /tmp/scripts/public.key
+                        singularity exec \\
+                            --cleanenv \\
+                            --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID},AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY},AWS_DEFAULT_REGION=us-east-1 \\
+                            docker://amazon/aws-cli \\
+                            aws ssm get-parameter --name \"/gpg/public-key\" --with-decryption --region us-east-1 --query Parameter.Value --output text > /tmp/scripts/public.key
                         gpg --import /tmp/scripts/public.key
                         gpg --verify /tmp/scripts/cancel_jobs.sh.sig /tmp/scripts/cancel_jobs.sh
                         if [ \$? -eq 0 ]; then
                             bash /tmp/scripts/cancel_jobs.sh ${SSH_USER} ${IMAGE}
                         else
-                            echo "GPG verification failed"
+                            echo \"GPG verification failed\"
                             exit 1
                         fi
                         rm -rf /tmp/scripts
-                    '
+                    "
                     """
                 }
             }
